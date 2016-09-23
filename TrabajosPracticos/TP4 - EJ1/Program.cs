3d6e0f09a781289b081ef2f8a557e05bc31fb3f0
AleOshiro <alejandro.oshiro.12@gmail.com>
2016-09-23 01:01:53 -0300

    Added TP4 - EJ1

---
+++ TrabajosPracticos/TP4 - EJ1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4___EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Materia> SextoCuatrimestre = new List<Materia>();
+
+            SextoCuatrimestre.Add(new Materia("Taller de computación VI", "Almiñana Gabriel", "Lunes", "18:40"));
+            SextoCuatrimestre.Add(new Materia("Seminario de tecnología", "Cudek Misael", "Lunes", "20:50"));
+            SextoCuatrimestre.Add(new Materia("Seguridad e integridad de sistemas", "Romanos Pablo", "Martes", "18:40"));
+            SextoCuatrimestre.Add(new Materia("Seminario de sistemas", "Romanos Pablo", "Miércoles", "18:40"));
+            SextoCuatrimestre.Add(new Materia("Computación Avanzada", "Breitman Alejandro", "Jueves", "18:40"));
+
+            foreach (Materia materia in SextoCuatrimestre)
+            {
+                Console.WriteLine("************************************************");
+                Console.WriteLine("*  Materia: " + materia._nombre);
+                Console.WriteLine("*  Profesor: " + materia._profesor);
+                Console.WriteLine("*  Día: " + materia._dia);
+                Console.WriteLine("*  Horario: " + materia._horario);
+                Console.WriteLine("************************************************");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
